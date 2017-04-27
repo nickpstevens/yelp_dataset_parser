60012b7c2ec7c6caf771c9d253b2ad1c71ca13aa
Nick Stevens <nickpstevens94@gmail.com>
2017-04-27 10:09:03 -0400

    Modified city field in output so that it can be used for stratification

--- src/filter_datasets.py
+++ src/filter_datasets.py
@@ -28,7 +28,10 @@ def filter_data(business_file, review_file):
                     entry = json.loads(line)
                     if re.match('(28|29|44|88|89|53|85|15|61)[0-9]{3}', entry['postal_code']):
                         business_ids.add(entry['business_id'])
-                        fout.write(line)
+                        entry['city'] = determine_city_from_business(entry)
+                        write_line = json.dumps(entry)
+                        fout.write(write_line)
+                        fout.write('\n')
                 except ValueError as e:
                     print e
     with open(review_file) as fin:
@@ -40,6 +43,25 @@ def filter_data(business_file, review_file):
                         fout.write(line)
                 except ValueError as e:
                     print e
+
+
+def determine_city_from_business(business):
+    if re.match('2[89][0-9]{3}', business['postal_code']):
+        return 'charlotte'
+    elif re.match('44[0-9]{3}', business['postal_code']):
+        return 'cleveland'
+    elif re.match('8[89][0-9]{3}', business['postal_code']):
+        return 'las_vegas'
+    elif re.match('53[0-9]{3}', business['postal_code']):
+        return 'madison'
+    elif re.match('85[0-9]{3}', business['postal_code']):
+        return 'phoenix'
+    elif re.match('15[0-9]{3}', business['postal_code']):
+        return 'pittsburgh'
+    elif re.match('61[0-9]{3}', business['postal_code']):
+        return 'urbana'
+    else:
+        return 'other'
 
 
 def main(args):
